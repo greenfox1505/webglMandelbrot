7aa843ffe765fcfd4a55c51ce83f25be47ae62eb
greenfox1505 <greenfox1505@gmail.com>
2016-01-18 07:56:14 +0000

    color controlls

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ var uniforms = {
 	},
 	color: {
 		type: "v3",
-		value: new THREE.Vector3(1, 0, 0)
+		value: new THREE.Vector3(0, 1, 0)
 	}
 }
 
@@ -47,8 +47,9 @@ camera.position.z = 1;
 var setURL = function(){
 	var i = window.location.origin + window.location.pathname +"?x="+n.center.x+
 		"&y="+n.center.y+
-		"&z="+n.zoom;
-		console.log(i)
+		"&z="+n.zoom + 
+		"&r="+uniforms.color.value.x+"&g="+uniforms.color.value.y+"&b="+uniforms.color.value.z;
+		//console.log(i)
 	window.history.replaceState( {} , "",i );
 
 }
@@ -76,11 +77,12 @@ var render = function() {
 	requestAnimationFrame(render);
 };
 
-document.onmousewheel = function(event) {
+renderer.domElement.onmousewheel = function(event) {
 	n.zoom += event.deltaY / 100;
-		setURL(n);
+	if(n.zoom > 30){n.zoom = 30}
+	setURL();
 }
-document.onmousemove = function(event) {
+renderer.domElement.onmousemove = function(event) {
 	if (event.buttons == 1) {
 		var z = Math.pow(1.05, n.zoom);
 		var delta = {
@@ -89,8 +91,12 @@ document.onmousemove = function(event) {
 		}
 		n.center.x -= delta.dx*z
 		n.center.y += delta.dy*z
-		setURL(n);
+		if(n.center.x > 2)n.center.x=2;
+		if(n.center.y > 2)n.center.y=2;
+		if(n.center.x < -2)n.center.x=-2;
+		if(n.center.y < -2)n.center.y=-2;
 	}
+	setURL();
 }
 
 render();
@@ -106,14 +112,10 @@ function getUrlVars() {
     });
     return vars;
   }
-
-
-
-
 var param = getUrlVars();
 //debugger;
-if(param.x){
 
+if(param.x){
 	n.center.x = parseFloat(param.x);
 }
 if(param.y){
@@ -122,3 +124,32 @@ if(param.y){
 if(param.z){
 	n.zoom= parseFloat(param.z);
 }
+if(param.r){
+	uniforms.color.value.x = parseFloat(param.r);
+}
+if(param.g){
+	uniforms.color.value.y = parseFloat(param.g);
+}
+if(param.b){
+	uniforms.color.value.z = parseFloat(param.b);
+}
+
+
+
+var myCont ={};
+myCont.red = uniforms.color.value.x;
+myCont.green = uniforms.color.value.y;
+myCont.blue = uniforms.color.value.z;
+myCont.loadColor = function(){
+	console.log(this.color);
+	uniforms.color.value.x = myCont.red
+	uniforms.color.value.y = myCont.green
+	uniforms.color.value.z = myCont.blue
+	console.log(uniforms.color.value);setURL()
+};
+var gui = new dat.GUI();
+gui.add(myCont,"red",0,1).onChange(myCont.loadColor);
+gui.add(myCont,"green",0,1).onChange(myCont.loadColor);
+gui.add(myCont,"blue",0,1).onChange(myCont.loadColor);
+
+//gui.add(myCont, 'loadColor');
